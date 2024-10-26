87d2bcfe2189b08f056458603cabcbe3cd2b9a58
BAN Sothen <ban.sothen2016@rupp.edu.kh>
2024-10-26 22:03:07 +0700

    added support module

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 # Read the contents of your README file
 from pathlib import Path
 readme = Path(__file__).parent / "README.md"
-long_description = readme.read_text()
+long_description = readme.read_text(encoding="utf-8")
 
 setup(
     name="bakong-khqr",
-    version="0.2.4",
+    version="0.2.5",
     author="BAN Sothen",
     author_email="bansokthen@gmail.com",
     description="A Python library for generating payment transactions compliant with the Bakong KHQR standard. (Unofficial NBC Library)",
@@ -15,13 +15,15 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/bsthen/bakong-khqr",
     packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "requests",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=[],
     include_package_data=True,
     project_urls={
         'Documentation': 'https://github.com/bsthen/bakong-khqr#readme',  # Replace with your documentation URL
